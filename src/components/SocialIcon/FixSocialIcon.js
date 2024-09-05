e718c692ceaa664fb828428f8a0ba37cb69f2af0
Muhammad Umar <muhammadumar8741@gmail.com>
2024-09-05 15:38:32 +0500

    Update FixSocialIcon.js

--- src/components/SocialIcon/FixSocialIcon.js
+++ src/components/SocialIcon/FixSocialIcon.js
@@ -58,7 +58,7 @@ function FixSocialIcon() {
         <ul>
           <li className="item">
             <a
-              href="https://www.linkedin.com/in/pedro-sales-muniz"
+              href="https://www.linkedin.com/in/zoreoz-software-club-undefined-2646b1327"
               target="_blank"
               rel="noopener noreferrer"
             >
@@ -67,7 +67,7 @@ function FixSocialIcon() {
           </li>
           <li className="item">
             <a
-              href="https://github.com/salesp07"
+              href="https://github.com/Zoreozsoftware"
               target="_blank"
               rel="noopener noreferrer"
             >
